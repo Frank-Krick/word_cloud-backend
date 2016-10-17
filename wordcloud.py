f84b4e352392165be19c6962e5a24eb4ca78c2ef
Frank Krick <frank.krick@gmail.com>
2016-10-16 23:55:07 -0400

    First test app

---
+++ wordcloud.py
@@ -0,0 +1,14 @@
+import falcon
+import json
+ 
+class QuoteResource:
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        quote = {
+            'quote': 'I\'ve always been more interested in the future than in the past.',
+            'author': 'Grace Hopper'
+        }
+        resp.body = json.dumps(quote)
+                                      
+app = falcon.API()
+app.add_route('/quote', QuoteResource())
